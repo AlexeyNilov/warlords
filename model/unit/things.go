36558cc89621848b5ae03aa58da122db15b24975
Alexey Nilov <alexeyn00000@gmail.com>
2025-05-23 21:31:11 +0200

    add army

--- model/unit/things.go
+++ model/unit/things.go
@@ -8,3 +8,21 @@ type Unit struct {
 func NewUnit(speed int, strength int) *Unit {
 	return &Unit{Speed: speed, Strength: strength}
 }
+
+type Army struct {
+	Units []*Unit
+	Speed int
+	Size int
+}
+
+func NewArmy() *Army {
+	return &Army{Speed: 0, Size: 0}
+}
+
+func (a *Army) AddUnit(unit *Unit) {
+	a.Units = append(a.Units, unit)
+	if a.Speed == 0 || unit.Speed < a.Speed {
+		a.Speed = unit.Speed
+	}
+	a.Size++
+}
--- model/unit/things_test.go
+++ model/unit/things_test.go
@@ -12,3 +12,24 @@ func TestUnit(t *testing.T) {
 	assert.Equal(t, 1, unit.Speed)
 	assert.Equal(t, 1, unit.Strength)
 }
+
+func TestArmy(t *testing.T) {
+	army := NewArmy()
+	assert.Equal(t, 0, army.Speed)
+	assert.Equal(t, 0, army.Size)
+}
+
+func TestAddUnit(t *testing.T) {
+	army := NewArmy()
+	army.AddUnit(NewUnit(1, 1))
+	assert.Equal(t, 1, army.Speed)
+	assert.Equal(t, 1, army.Size)
+}
+
+// Army speed equals to speed of the slowest unit
+func TestArmySpeed(t *testing.T) {
+	army := NewArmy()
+	army.AddUnit(NewUnit(1, 1))
+	army.AddUnit(NewUnit(2, 1))
+	assert.Equal(t, 1, army.Speed)
+}
